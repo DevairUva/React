df6909afd24457314b72f3d01e2a378ef738dcbf
Devair Martins <devair.martins@gmail.com>
2022-06-14 11:10:49 -0300

    coisa linda

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vamos aprender!<br></br>
+          Coisa linda!<br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
