80368cdfafc55f2dd451e088daf15f7f3c1a2b71
Devair Martins <devair.martins@gmail.com>
2022-06-14 11:06:04 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Vamos aprender!<br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
